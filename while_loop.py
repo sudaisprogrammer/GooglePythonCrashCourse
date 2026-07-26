863bdf7c6bf832c7cdb241a0acb7744bac35d9e4
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-26 10:37:41 +0500

    while loop in python

--- while_loop.py
+++ while_loop.py
@@ -4,3 +4,12 @@ while(x<5):
     print("Not there yet, x = "+str(x))
     x+=1;
 print("x="+str(x))
+
+#while loop in python execute the code until the specified condition not meet but thats not always true
+# in some cases we need infinite loop for our tasks such as servers which are ON 24/7 so we will use loop to complete our job
+
+#infinit while loop
+y = 1
+while(y):
+    print("y = "+str(y))
+    y+=1;
