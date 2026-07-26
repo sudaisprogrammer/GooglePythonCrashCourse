f608f69355779b2cdb7107ce107bff5d7fc6f8c8
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-26 09:44:01 +0500

    logical operators

---
+++ logical-operators.py
@@ -0,0 +1,8 @@
+#like in digital logic design subject we can also use booleans and logical operators like and,or, not and more
+
+google = True
+meta = False
+
+print(not google) #false
+print(meta or google) #google true
+print(meta and google) #true if both are true else false
