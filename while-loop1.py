5f78d6f1b8e06e769f148bbc48ed5e5ed7e1677f
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-27 13:12:16 +0500

    some changes in program

--- while-loop1.py
+++ while-loop1.py
@@ -1,7 +1,7 @@
 
 def check_attempts(password):
     total_attempts = 4
-    x = 0
+    x = 1
     
     while(True):
         mypass = input("enter password: ")
@@ -10,7 +10,7 @@ def check_attempts(password):
             print("remaining attempts are "+str(total_attempts-(x+1)))
             x+=1;
         else:
-            print("password mathced")
+            print("password mathced in ",x," attempts ")
             break;
 
 check_attempts("pass123")
