53523c3dc9d1b23699c71715b0850a8da2159704
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-27 13:09:12 +0500

    basic of strings in python

---
+++ strings.py
@@ -0,0 +1,11 @@
+name =  "above the sky"
+
+print(name[0:5:3]) #like range fucntion we can also do things like start,end,steps
+
+print(name[-6]) #negataitve index always count from back to front 
+
+for c in name:
+    print(c)
+
+names = ["the ","world ","is ","broken"]
+print("".join(name)) #join method joins the list into single things
