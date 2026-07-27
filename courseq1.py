46b8180a3488291fa199de1f2c9822dff65db5ca
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-27 13:07:18 +0500

    Course Q1 solved

--- courseq1.py
+++ courseq1.py
@@ -5,6 +5,6 @@ def is_power(number,base):
         return 1
     return is_power(number/base,base)
 
-print(is_power(8,2))
+print(is_power(8,2)) # as 2^3 = 8 return 1
 print(is_power(16,2))
 print(is_power(3666,2))
