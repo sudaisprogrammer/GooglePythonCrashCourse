3bc4760673982d27f0b841d59566c77688bf55b7
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-28 22:53:47 +0500

    left triangle

--- shape2.py
+++ shape2.py
@@ -1,12 +1,10 @@
 def lefttriangle(rows):
     space = rows
-    for i in range(rows):
+    for i in range(rows+1):
         for j in range(space-i):
-
-
-
-
-
-
+            print(" ",end=" ")
+        for k in range(i):
+            print("*",end=" ")
+        print(" ")
 
 lefttriangle(4)
