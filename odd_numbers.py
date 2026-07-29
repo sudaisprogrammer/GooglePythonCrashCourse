1b6464dcb762018ece290544f39e685c696cc72a
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-29 22:45:23 +0500

    find maximum odd numbers

--- odd_numbers.py
+++ odd_numbers.py
@@ -1,3 +1,4 @@
+#program to find maximum odd numbers like if num = 10 then output must be 1,3,5,7,9 and so on
 def maximum_odd(num):
     return_string = ""
     for i in range(1,num):
@@ -6,6 +7,6 @@ def maximum_odd(num):
     return return_string.rstrip()
 
 
-print(maximum_odd(6))
+print(maximum_odd(6)) 
 print(maximum_odd(12))
 print(maximum_odd(123))
