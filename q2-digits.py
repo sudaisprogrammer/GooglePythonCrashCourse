4763b11dda0dbc5a1eb438b5ab7de682c9e18d43
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-29 22:17:23 +0500

    Digits finder

---
+++ q2-digits.py
@@ -0,0 +1,13 @@
+def digits(num):
+    count = 0
+    n = num
+    # if num == 0:
+    #     count+=1
+    while (n!=0):
+        num = n%10
+        n = n//10
+        count+=1
+    return count
+
+n = digits(3411)
+print(n)
