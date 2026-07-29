1f60d0e5d0a934ccccd8841ae063f54b2123c0dc
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-29 22:44:23 +0500

    find maximum odd numbers

--- odd_numbers.py
+++ odd_numbers.py
@@ -1,9 +1,11 @@
 def maximum_odd(num):
     return_string = ""
     for i in range(1,num):
-        if num%2!=0:
+        if i%2!=0:
             return_string +=f",{i}"
     return return_string.rstrip()
 
 
 print(maximum_odd(6))
+print(maximum_odd(12))
+print(maximum_odd(123))
