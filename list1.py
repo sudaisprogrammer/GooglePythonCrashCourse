12c6f0f3f369c4b1f3cd3288732446f5a6d95be7
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-31 21:28:09 +0500

    List in python

---
+++ list1.py
@@ -0,0 +1,15 @@
+
+x = [5, 10, 15, 20]
+
+names = ["sudais", "ahmad", "ali", "hassan"]
+
+print(type(x)) #print list
+print(type(names)) #print list
+
+print(x[0]) #print first element of list
+print(x[1]) #print second element of list
+print(names[0]) #print first element of list
+
+print(x[-1]) #print last element of list
+
+print(x[0:2]) #print first two elements of list
