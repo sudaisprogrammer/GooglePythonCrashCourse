b2d3081ed8bf0b8a6fbcfdc01e24d3d5b5f54429
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-31 21:33:45 +0500

    tuple in python

---
+++ tuple.py
@@ -0,0 +1,8 @@
+#tuple in python
+#tuples are immutable data types in python, which means that once a tuple is created,  it cannot be modified. Tuples are defined using parentheses () and can contain elements of different data types, including other tuples.
+
+frutis = ("apple","banana","cherry","kiwi","mango")
+print(frutis)
+
+print(frutis.count("apple")) #print number of times "apple" appears in the tuple
+print(frutis.index("kiwi")) #print the index of "kiwi" in the tuple
