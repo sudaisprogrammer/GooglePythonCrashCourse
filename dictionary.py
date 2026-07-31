542d12e026d633b44f6c086f547e5c8a4d42c144
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-31 21:26:46 +0500

    Dictionary in python

---
+++ dictionary.py
@@ -0,0 +1,24 @@
+#dictionary in python
+
+x = {}
+print(type(x))
+
+file_count = {"jpg":10,"cpp":12,"java":33,"csv":10}
+
+print(file_count)
+
+#check if jpg in file_count
+
+print("jpg" in file_count)
+print("py" in file_count)
+
+#update value of key
+
+file_count["jpg"] = 19
+print(file_count)
+
+#iterate key value
+
+for key,value in file_count.items():
+    print(key,value)
+
