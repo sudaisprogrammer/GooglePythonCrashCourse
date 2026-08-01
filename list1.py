0df916a98cde1c31901d64bcfcb320d268395263
sudaisprogrammer <sudais.programmer@gmail.com>
2026-08-01 22:31:51 +0500

    updated list1

--- list1.py
+++ list1.py
@@ -4,7 +4,7 @@ x = [5, 10, 15, 20]
 names = ["sudais", "ahmad", "ali", "hassan"]
 
 print(type(x)) #print list
-print(type(names)) #print list
+print(type(names)) #print the list whole list
 
 print(x[0]) #print first element of list
 print(x[1]) #print second element of list
