dd26dbcc7f57abb65302fe9a2995ceea65461ed9
sudaisprogrammer <sudais.programmer@gmail.com>
2026-08-01 22:34:15 +0500

    google course question

--- q2.py
+++ q2.py
@@ -1,12 +1,10 @@
 # Question 4
 # Fill in the gaps in the nametag function so that it uses the format method to return first_name and the first initial of last_name followed by a period. For example, nametag("Jane", "Smith") should return "Jane S."
 
-def nametag(first_name, last_name):
+def nametag(first_name, last_name): #function to return first name and first initial of last name
     return("{} {}.".format(first_name,last_name[0]))
 
 
 print(nametag("Jane", "Smith")) 
-
 print(nametag("Francesco", "Rinaldi")) 
-
 print(nametag("Jean-Luc", "Grand-Pierre")) 
