ffec09a9ea2e4823391edd3901a41732aa463d36
sudaisprogrammer <sudais.programmer@gmail.com>
2026-08-01 22:35:15 +0500

    google course question

--- q3.py
+++ q3.py
@@ -17,7 +17,7 @@ def replace_ending(sentence, old, new):
 print(replace_ending("It's raining cats and cats", "cats", "dogs")) 
 # Should display "It's raining cats and dogs"
 print(replace_ending("She sells seashells by the seashore", "seashells", "donuts")) 
-# Should display "She sells seashells by the seashore"
+
 print(replace_ending("The weather is nice in May", "may", "april")) 
 
 print(replace_ending("The weather is nice in May", "May", "April")) 
