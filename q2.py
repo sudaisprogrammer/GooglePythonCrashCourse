55c01e11e63c3db988fbef200a8466f7d7736e34
sudaisprogrammer <sudais.programmer@gmail.com>
2026-08-01 22:33:43 +0500

    google course question

--- q2.py
+++ q2.py
@@ -6,8 +6,7 @@ def nametag(first_name, last_name):
 
 
 print(nametag("Jane", "Smith")) 
-# Should display "Jane S." 
+
 print(nametag("Francesco", "Rinaldi")) 
-# Should display "Francesco R." 
+
 print(nametag("Jean-Luc", "Grand-Pierre")) 
-# Should display "Jean-Luc G." 
