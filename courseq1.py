8a06cf111365f2cfaa9c3281180891997e76e2c7
sudaisprogrammer <sudais.programmer@gmail.com>
2026-08-02 22:38:35 +0500

    google course q1

--- courseq1.py
+++ courseq1.py
@@ -8,3 +8,4 @@ def is_power(number,base):
 print(is_power(8,2)) # as 2^3 = 8 return 1
 print(is_power(16,2)) # 2^4 = 16 return 1
 print(is_power(64,3)) # as there is no number which 3^ = 64 return 0 false
+
