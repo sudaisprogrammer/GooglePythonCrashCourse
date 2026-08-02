a964acd2db47daa0eb92c5efb64c71dd06b0f889
sudaisprogrammer <sudais.programmer@gmail.com>
2026-08-02 22:42:37 +0500

    check if a string is palindrome or not

--- palindrome.py
+++ palindrome.py
@@ -28,7 +28,6 @@ def is_palindrome(input_string):
     # Otherwise, return False.
     return False
 
-
 print(is_palindrome("Never Odd or Even")) # Should be True
 print(is_palindrome("abc")) # Should be False
 print(is_palindrome("kayak")) # Should be True
