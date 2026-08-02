776c559f579a88f0221b6df8bd6bf56d77306048
sudaisprogrammer <sudais.programmer@gmail.com>
2026-08-02 22:42:06 +0500

    google course question

--- q5.py
+++ q5.py
@@ -2,7 +2,6 @@ filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.o
 # Generate new_filenames as a list containing the new filenames
 # using as many lines of code as your chosen method requires.
 
-
 new_filenames = []
 for filename in filenames:
     if filename.endswith("hpp"):
