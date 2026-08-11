923b86b0a03102094d888ae3163dce957f632c20
sudaisprogrammer <sudais.programmer@gmail.com>
2026-08-11 22:55:55 +0500

    google crash python course practise questions

--- q2.py
+++ q2.py
@@ -7,3 +7,4 @@ def nametag(first_name, last_name): #function to return first name and first ini
 print(nametag("Jane", "Smith")) 
 print(nametag("Francesco", "Rinaldi")) 
 print(nametag("Jean-Luc", "Grand-Pierre"))
+
