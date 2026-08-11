63165cdeb5c6fbee52b51bdf236d80e96b76fd62
sudaisprogrammer <sudais.programmer@gmail.com>
2026-08-11 22:55:33 +0500

    google crash python course practise questions

--- q3.py
+++ q3.py
@@ -20,3 +20,4 @@ print(replace_ending("She sells seashells by the seashore", "seashells", "donuts
 print(replace_ending("The weather is nice in May", "may", "april")) 
 
 print(replace_ending("The weather is nice in May", "May", "April"))
+
